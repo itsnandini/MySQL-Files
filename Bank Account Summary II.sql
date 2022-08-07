209eab85b18ea18a72c43ef82770cd2dcbf475de
Nandini Agarwal <104410750+itsnandini@users.noreply.github.com>
2022-08-07 10:51:48 +0530

    Create Bank Account Summary II.sql

---
+++ Bank Account Summary II.sql
@@ -0,0 +1,10 @@
+select u.name, c.balance
+from Users u
+right join
+(
+    select account, sum(amount) as balance
+    from Transactions
+    group by 1
+) c
+on u.account = c.account
+where balance > 10000;
